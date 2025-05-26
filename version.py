b4042702bc2a696a5485cab5318f0a3308f3183b
MasterRyped <136719697+Github-Taha@users.noreply.github.com>
2025-05-26 08:44:13 -0400

    Update version.py

--- version.py
+++ version.py
@@ -5,13 +5,12 @@
 # Monday, May 26, 2025
 #
 # Is the version and init data for the program
-# Version: 4.0
 #=================================================
 
 VERSION = "4.0"
+GIT_URL = 'https://raw.githubusercontent.com/Github-Taha/MakeFile/main/version.py'
 DEFAULT_NAME = "Taha Amir"
 TAB = "    "
 ASSIGNMENT = "Assignments"
 UNIT = "Unit "
 PRACTICE_PROGRAMS = "Practice Programs "
-
