81d39850357ca6e7c9d8e10773bf006382e143fd
MasterRyped <136719697+Github-Taha@users.noreply.github.com>
2025-05-26 08:32:01 -0400

    Update version.py

--- version.py
+++ version.py
@@ -5,7 +5,13 @@
 # Monday, May 26, 2025
 #
 # Is the version and init data for the program
+# Version: 4.0
 #=================================================
 
 VERSION = "4.0"
+DEFAULT_NAME = "Taha Amir"
+TAB = "    "
+ASSIGNMENT = "Assignments"
+UNIT = "Unit "
+PRACTICE_PROGRAMS = "Practice Programs "
 
