7781f27117a6a138d9f72b08f8d62bc285aef134
MasterRyped <136719697+Github-Taha@users.noreply.github.com>
2025-05-26 12:10:36 -0400

    Create make_file.py

---
+++ make_file.py
@@ -0,0 +1,58 @@
+#=================================================
+# ------------------- Make File ------------------
+#
+# Taha Amir
+# Tuesday, May 13, 2025
+#
+# Make a new Python file with the neccessary
+# options on it.
+#=================================================
+
+# Check for updates
+import update_manager
+import urllib.request
+
+if update_manager.update_needed:
+    print("Updating...")
+
+    success = True
+
+    try:
+        import version2
+        # Load up all the files
+        filePathInit = 'https://raw.githubusercontent.com/Github-Taha/MakeFile/main/'
+        for files in version2.FILES:
+            filePath = filePathInit + files
+
+            url = filePath
+            save_path = filePath
+
+            try:
+                urllib.request.urlretrieve(url, files)
+                print(f"File {files} downloaded.")
+            except Exception as e:
+                print(f"Failed to download file: {e}")
+                success = False
+
+        # If Download Successful
+        if success:
+            print("\n")
+            print("Update Completed Successfully!")
+
+            # Start the program
+            input("\n\nPress [Enter] to continue...")
+            import main
+
+        else:
+            print("\n")
+            print("Update could not complete.")
+    except AttributeError:
+        print("Programming Error. Starting program.")
+
+        # Start the program
+        input("\n\nPress [Enter] to continue...")
+        import main
+else:
+    # Start the program
+    input("\n\nPress [Enter] to continue...")
+    import main
